2e90bcb84addaff8e44bf0a7b387e8ed329a9b9d
LULUYOU\zhangx <xiaozhang0210@hotmail.com>
2018-02-08 15:21:35 +0800

    Owin 框架搭建成功

---
+++ Demo/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace Demo.Controllers
+{
+    [RoutePrefix("v0/users")]
+    public class UsersController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public string GetAllUsers()
+        {
+            var listUsers = new List<dynamic>
+            {
+                new
+                {
+                    Name = "wawa0210",
+                    Gender = "M"
+                }
+            };
+
+            return JsonConvert.SerializeObject(listUsers);
+        }
+    }
+}
---
+++ Demo/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+[assembly: OwinStartup(typeof(Demo.Startup))]
+
+namespace Demo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            var configuration = new HttpConfiguration(); configuration.Formatters.Remove(configuration.Formatters.XmlFormatter);
+            configuration.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            configuration.MapHttpAttributeRoutes();
+            app.UseWebApi(configuration);
+        }
+    }
+}
